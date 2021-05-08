8e0540cfc5351b2b6ae09db79a7a6521bb16aedf
williamzhou8818 <william.zhou8818@gmail.com>
2021-05-08 16:57:26 +1000

    add the image url

--- routers/auth/index.js
+++ routers/auth/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {v4: uuidv4} = require('uuid');
 
 
+
 /**
  * @route GET api/auth
  * @desc Get logged  in user
@@ -15,14 +16,26 @@ const {v4: uuidv4} = require('uuid');
      ip = add;
  })
 
+
  router.get('/', (req, res) => { 
 
-    const tempObj = {
-        user_id: uuidv4(),
-        local_ip: ip,
-        port: "5500",
-        msg: "this is a test"
-    }
+    const tempObj = [
+        {
+            user_id: uuidv4(),
+            local_ip: ip,
+            port: "5500",
+            image: `http://${req.headers.host}/001-00.png`,
+            title: "test one",
+         },
+         {
+            user_id: uuidv4(),
+            local_ip: ip,
+            port: "5500",
+            image: `http://${req.headers.host}/00005-00.png`,
+            title: "test two",
+         },
+
+    ]
 
     res.status(200).json({auth: tempObj });
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
@@ -10,6 +11,9 @@ app.use(cors());
 app.use(express.json({limit: '500mb'}));
 app.use(express.urlencoded({limit: '520mb', extended:false}));
 
+//public folder
+let publicFolder = path.join(__dirname, 'public');
+app.use('/', express.static(publicFolder));
 
 
 //router
@@ -17,6 +21,7 @@ app.use('/api/v1/auth', require('./routers/auth'));
 
 //main root
 app.use('/', (req, res) =>  {
+
     res.status(200).json({msg: 'Welcome to PGM Aphoro3D Api'});
 });
 //listen port 5500
